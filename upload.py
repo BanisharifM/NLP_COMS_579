9afae9d7f8a655631fd083398736e32b4de33dff
Null <Null@MacBook-Air-2.local>
2024-04-05 01:21:10 -0500

    pdf file now is loaded

--- upload.py
+++ upload.py
@@ -1,9 +1,27 @@
 import os
 import weaviate
 from dotenv import load_dotenv
+from pathlib import Path
+import argparse
+from llama_index.readers.file import PDFReader
+
+
+def load_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--pdf_file", type=str, required=True, help="pdf file path")
+    parser.add_argument("--chunk_size", type=int, default=500, help="chunk size")
+    args = parser.parse_args()
+    return args
+
+
+def read_pdf(path):
+    loaded_pdf = PDFReader().load_data(Path(path))
+    return loaded_pdf
 
 
 def main():
+    # Load Arguments
+    args = load_arguments()
 
     # Load environment variables from .env file
     load_dotenv()
@@ -17,6 +35,10 @@ def main():
 
     # Check connection to the weaviate
     print(client.is_ready())
+
+    # Load pdf file
+    loaded_file = read_pdf(args.pdf_file)
+    print(f"number of pages: {len(loaded_file)}")
 
 
 if __name__ == "__main__":
