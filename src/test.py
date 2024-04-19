ce50acd83ef986c1e9a10371d82aac0c93f6eaed
sadegh jafari <sadeghjafari@mac-air-144.cs.iastate.edu>
2024-04-18 23:12:39 -0500

    move test.py

--- src/test.py
+++ src/test.py
@@ -6,3 +6,38 @@ client = OpenAI()
 # client = OpenAI(
 #   api_key=os.environ.get("CUSTOM_ENV_NAME"),
 # )
+
+from openai import OpenAI
+
+client = OpenAI()
+
+completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {
+            "role": "system",
+            "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair.",
+        },
+        {
+            "role": "user",
+            "content": "Compose a poem that explains the concept of recursion in programming.",
+        },
+    ],
+)
+
+print(completion.choices[0].message)
+
+response = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Who won the world series in 2020?"},
+        {
+            "role": "assistant",
+            "content": "The Los Angeles Dodgers won the World Series in 2020.",
+        },
+        {"role": "user", "content": "Where was it played?"},
+    ],
+)
+
+print(response)
