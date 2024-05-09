1c74b5e28ba6f9b5e26ae3935f7a354c9337c201
Null <Null@MacBook-Air-4.local>
2024-05-09 18:21:40 -0500

    update the streamlit app with new UI

--- src/app.py
+++ src/app.py
@@ -1,4 +1,12 @@
+# src/app.py
+# import sys
+# import os
+
+# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "utils")))
+
 import streamlit as st
+from weaviate_client import create_client
+
 
 # if st.button("Home"):
 #     st.switch_page("your_app.py")
@@ -24,26 +32,23 @@ def main():
 
     # Main content area
     st.subheader("Client Credentials")
-    cluster_url = st.text_input("Weaviate Cluster URL", "WCS_CLUSTER_URL")
+    weaviate_url = st.text_input("Weaviate Cluster URL", "WCS_CLUSTER_URL")
     weaviate_api_key = st.text_input(
         "Weaviate API keys", "WCS_API_KEY", type="password"
     )
-    client_secret = st.text_input(
-        "Client Secret", "Enter Client Secret", type="password"
-    )
+    # client_secret = st.text_input(
+    #     "Client Secret", "Enter Client Secret", type="password"
+    # )
 
     # Token information
     token_info = st.empty()  # Placeholder for token info
-    if st.button("Generate Token"):
-        token = "Bearer/JWT token generated. It will expire in 30 minutes. To-regenerate, please call acquire_jwt_token()."
-        token_info.info(token)
-
-    # Options
-    st.checkbox("From CLI")
-    st.checkbox("Continuously Run")
-
-    # Footer
-    st.text("Powered by Funix.io, minimally building apps in Python")
+    if st.button("Connect to Weaviate"):
+        print(weaviate_url)
+        print(weaviate_api_key)
+        client = create_client()
+        if client.is_ready():
+            msg = "Client is successfully connected to Weaviate and ready to use."
+        token_info.info(msg)
 
 
 if __name__ == "__main__":
