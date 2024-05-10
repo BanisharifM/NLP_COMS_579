ad76a7d7db538b46fc0d06bd0aebe5f6fe791b31
Null <Null@MacBook-Air-4.local>
2024-05-10 00:58:57 -0500

    Big update on Streamlit App UI

--- src/app.py
+++ src/app.py
@@ -1,70 +1,29 @@
-# src/app.py
-# import sys
-# import os
-
-# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "utils")))
-
 import streamlit as st
-from weaviate_client import create_client
-import os
-
-
-# if st.button("Home"):
-#     st.switch_page("your_app.py")
-# if st.button("Page 1"):
-#     st.switch_page("pages/page_1.py")
-# if st.button("Page 2"):
-#     st.switch_page("pages/page_2.py")
-
-# import streamlit as st
+import importlib
 
 
 def main():
-    st.title("Initialize Weaviate")
-
-    # Sidebar for input
     with st.sidebar:
-        st.header("Functions / Pages")
-        st.markdown("* Initialize Weaviate")
-        st.markdown("* Initialize Llama")
-        st.markdown("* Create corpus")
-        st.markdown("* Reset corpus")
-        st.markdown("* Upload file")
-        st.markdown("* Query")
+        st.header("RAG Application")
+        page = st.radio(
+            "Go to",
+            [
+                "Initialize Weaviate",
+                "Upload file",
+                "Query",
+            ],
+        )
 
-    # Main content area
-    st.subheader("Client Credentials")
-    weaviate_url = st.text_input("Weaviate Cluster URL", "")
-    weaviate_api_key = st.text_input("Weaviate API keys", "", type="password")
-    # client_secret = st.text_input(
-    #     "Client Secret", "Enter Client Secret", type="password"
-    # )
-    # st.write(f"RUL:  {weaviate_url}")
-    # st.write(f"API:  {weaviate_api_key}")
-    # Token information
-    # token_info = st.empty()  # Placeholder for token info
-    if st.button("Connect to Weaviate"):
-        env_path = os.path.join(os.path.dirname(__file__), "..", ".env")
-        with open(env_path, "r") as file:
-            lines = file.readlines()
+    page_to_module = {
+        "Initialize Weaviate": "initialize_weaviate",
+        "Upload file": "upload_file",
+        "Query": "query",
+    }
 
-        with open(env_path, "w") as file:
-            for line in lines:
-                if line.startswith("WCS_CLUSTER_URL"):
-                    file.write(f"WCS_CLUSTER_URL='{weaviate_url}'\n")
-                elif line.startswith("WCS_API_KEY"):
-                    file.write(f"WCS_API_KEY='{weaviate_api_key}'\n")
-                else:
-                    file.write(line)
-
-        st.info("Environment variables updated successfully!")
-        client = create_client()
-        if client and client.is_ready():
-            st.success("Client is successfully connected to Weaviate and ready to use.")
-        else:
-            st.error(
-                "Failed to connect to Weaviate. Please check the connection settings or ensure the API key is valid."
-            )
+    if page in page_to_module:
+        module_name = page_to_module[page]
+        module = importlib.import_module(f"modules.{module_name}")
+        module.run()
 
 
 if __name__ == "__main__":
